50d9d40a2edcce0e0ce25d80ed902db8298f68a3
DilankaSanjula <dilankasanjula@gmail.com>
2023-09-10 22:07:11 +0530

    Final version

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ if "messages" not in st.session_state:
     st.session_state.messages = []
     
 for message in st.session_state.messages:
-    with st.chat(message["role"]):
+    with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
 
