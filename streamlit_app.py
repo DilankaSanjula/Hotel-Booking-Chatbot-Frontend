2f511a2506d9cad5a9064f961c33db5335f3f427
DilankaSanjula <dilankasanjula@gmail.com>
2023-09-10 21:49:17 +0530

    Testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,15 +9,16 @@ for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
 if prompt := st.chat_input():
-    st.write(f"User has sent the following prompt: {prompt}")
+    #st.write(f"User has sent the following prompt: {prompt}")
     # if not openai_api_key:
     #     st.info("Please add your OpenAI API key to continue.")
     #     st.stop()
 
     # openai.api_key = openai_api_key
-    # st.session_state.messages.append({"role": "user", "content": prompt})
-    # st.chat_message("user").write(prompt)
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
     # response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
     # msg = response.choices[0].message
-    # st.session_state.messages.append(msg)
-    # st.chat_message("assistant").write(msg.content)
+    msg = "testss"
+    st.session_state.messages.append(msg)
+    st.chat_message("assistant").write(msg.content)
