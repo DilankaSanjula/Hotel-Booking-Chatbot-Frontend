0c210dd3393bfa145f3f00b23c6f0098115ac868
DilankaSanjula <dilankasanjula@gmail.com>
2023-09-15 11:04:49 +0530

    Added metrics

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,26 @@
 import streamlit as st
 import requests
+import time
+
+
+class Metrics:
+    """
+    Handles metrics
+    """
+
+    def __init__(self):
+        self.total_queries = 0
+        self.total_response_time = 0
+
+    def metrics_avg_response_time(self, query_start_time):
+        query_end_time = time.time()
+        query_response_time = query_end_time - query_start_time
+        self.total_queries += 1
+        self.total_response_time += query_response_time
+        return self.total_response_time, self.total_queries 
+
+
+metrics = Metrics()
 
 api_url = "http://localhost:5005/webhooks/rest/webhook"
 
@@ -12,12 +33,16 @@ if "messages" not in st.session_state:
 #Title
 st.title("ABC Hotel Chatbot")
 
+
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
 #Display existing chat messages
 if prompt := st.chat_input():
+
+    query_start_time = time.time()
+
     # User message added to the list of messages in session state
     with st.chat_message("user"):
         st.markdown(prompt)
@@ -39,6 +64,18 @@ if prompt := st.chat_input():
 
         with st.chat_message("assistant"):
             st.markdown(response)
-
-        # Assistant message added to the list of messages
-        st.session_state.messages.append({"role": "assistant", "content": response})
+            
+    total_response_time, total_queries  = metrics.metrics_avg_response_time(query_start_time)
+    if total_queries > 0:
+        average_response_time = total_response_time / total_queries
+        st.sidebar.subheader("Total Number of Queries Handled")
+        st.sidebar.write(f"{str(total_queries)}")
+        st.sidebar.subheader("Average Response Time")
+        st.sidebar.write(f"{average_response_time:.2f} seconds")
+        
+    else:
+        st.sidebar.write("No queries yet.")
+        
+    # Assistant message added to the list of messages
+    st.session_state.messages.append({"role": "assistant", "content": response})
+    
