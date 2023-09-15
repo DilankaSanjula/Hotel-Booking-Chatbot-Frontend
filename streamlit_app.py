74d808dbc3ee92e693e1250b342aad19de2a818c
DilankaSanjula <dilankasanjula@gmail.com>
2023-09-15 11:09:16 +0530

    Metrics updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,8 @@ import time
 
 class Metrics:
     """
-    Handles metrics
+    Handles response time metric
     """
-
     def __init__(self):
         self.total_queries = 0
         self.total_response_time = 0
@@ -15,10 +14,8 @@ class Metrics:
     def metrics_avg_response_time(self, query_start_time):
         query_end_time = time.time()
         query_response_time = query_end_time - query_start_time
-        self.total_queries += 1
-        self.total_response_time += query_response_time
-        return self.total_response_time, self.total_queries 
-
+      
+        return query_response_time 
 
 metrics = Metrics()
 
@@ -32,7 +29,6 @@ if "messages" not in st.session_state:
 
 #Title
 st.title("ABC Hotel Chatbot")
-
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
@@ -51,30 +47,24 @@ if prompt := st.chat_input():
     payload = {
         "message": prompt
     }
-
+    
+    #post request to rasa enable api. Chatbot connection
     response = requests.post(api_url, json=payload)
     chatbot_response = response.json()
     print(chatbot_response)
     
     #Handling multiple bot responses
     for responses in chatbot_response:
-        print(responses['text'])
         bot = responses['text']
         response = f"ABC: {bot}"
 
         with st.chat_message("assistant"):
             st.markdown(response)
             
-    total_response_time, total_queries  = metrics.metrics_avg_response_time(query_start_time)
-    if total_queries > 0:
-        average_response_time = total_response_time / total_queries
-        st.sidebar.subheader("Total Number of Queries Handled")
-        st.sidebar.write(f"{str(total_queries)}")
-        st.sidebar.subheader("Average Response Time")
-        st.sidebar.write(f"{average_response_time:.2f} seconds")
-        
-    else:
-        st.sidebar.write("No queries yet.")
+    # Side bar to display response time of each query
+    response_time = metrics.metrics_avg_response_time(query_start_time)
+    st.sidebar.subheader("Response Time")
+    st.sidebar.write(f"{response_time:.2f} seconds")
         
     # Assistant message added to the list of messages
     st.session_state.messages.append({"role": "assistant", "content": response})
